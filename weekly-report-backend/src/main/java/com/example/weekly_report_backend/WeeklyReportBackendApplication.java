0e2fbe284e1d2d6699d12f9738b0dd1aaa9f51d0
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-07-06 14:34:47 +0530

    Initial backend setup

---
+++ weekly-report-backend/src/main/java/com/example/weekly_report_backend/WeeklyReportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.weekly_report_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyReportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyReportBackendApplication.class, args);
+	}
+
+}
---
+++ weekly-report-backend/src/test/java/com/example/weekly_report_backend/WeeklyReportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weekly_report_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyReportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
